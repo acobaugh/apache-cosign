37719ab08812af49938d1e148358e444fdd90ba6
Andy Cobaugh <andrewcobaugh@gmail.com>
2017-09-01 16:40:51 -0400

    initial commit

---
+++ start.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+
+# Redirect logs to stdout and stderr for docker reasons.
+ln -sf /dev/stdout /usr/local/apache2/logs/access_log
+ln -sf /dev/stderr /usr/local/apache2/logs/error_log
+
+/usr/local/apache2/bin/httpd -DFOREGROUND 
